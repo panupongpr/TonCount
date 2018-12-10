40efdc18d18f3c13a4b752b6b00f308b5b54d170
ton <panupong.pspd@gmail.com>
2018-12-10 10:44:30 +0700

    start app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'TonCounting';
-}
+export class AppComponent implements OnInit{
+  
+  // Explicit 
+  private myTitle: string;
+  private titleColor: string;
+  private myNumber;
+
+  constructor(){}
+
+  ngOnInit(): void {
+    
+   this.myTitle = 'Came Countting';
+   this.titleColor = '#FF0000';
+   this.myNumber = 0;
+
+  }
+
+
+}// Main Class
